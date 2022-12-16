0a3e67d4ddc536ef9f0ac3888bb263066f0b5b0c
dvlprJobayer <nesarahammed019@gmail.com>
2022-12-16 21:15:38 +0600

    some changes on readme file

--- src/api/axiosBicycle.js
+++ src/api/axiosBicycle.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
 const axiosBicycle = axios.create({
-    baseURL: 'https://pure-mesa-49301.herokuapp.com'
+  baseURL: "https://bicycle-builder.onrender.com",
 });
 
 // Add a request interceptor
-axiosBicycle.interceptors.request.use(function (config) {
+axiosBicycle.interceptors.request.use(
+  function (config) {
     // Do something before request is sent
     if (!config.headers.authorization) {
-        config.headers.authorization = `Bearer ${localStorage.getItem('accessToken')}`
+      config.headers.authorization = `Bearer ${localStorage.getItem(
+        "accessToken"
+      )}`;
     }
     return config;
-}, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-});
+  }
+);
 
 export default axiosBicycle;
